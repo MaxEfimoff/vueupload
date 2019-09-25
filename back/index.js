14228823030db0c6c5b5f32281e2a7d17d34f0c0
Max Efimoff <emv3@ya.ru>
2019-09-25 18:42:57 +0400

    server validation added

--- back/index.js
+++ back/index.js
@@ -4,14 +4,43 @@ const multer = require('multer');
 
 const app = express();
 
+const fileFilter = function(req, file, cb) {
+  const allowedTypes = ['image/jpeg', 'image/png'];
+
+  if (!allowedTypes.includes(file.mimetype)) {
+    const error = new Error('Wrong file type');
+    error.code = 'LIMIT_FILE_TYPES';
+    return cb(error, false);
+  }
+
+  cb(null, true);
+}
+
+const MAX_SIZE = 140000;
 const upload = multer({
-  dest: './uploads'
+  dest: './uploads',
+  fileFilter,
+  limits: {
+    fileSize: MAX_SIZE
+  }
 })
 
 // 'file' comes from front/src/SimpleUpload.vue 
 // 33 line formData.append('file', this.file);
 app.post('/upload', upload.single('file'), (req, res) => {
   res.json({file: req.file});
+})
+
+app.use(function(err, req, res, next) {
+  if(err.code === 'LIMIT_FILE_TYPES') {
+    res.status(422).json({error: 'Only images are allowed'});
+    return;
+  }
+
+  if(err.code === 'LIMIT_FILE_SIZE') {
+    res.status(422).json({error: `Only ${MAX_SIZE/1000} Kb are allowed`});
+    return;
+  }
 })
 
 
