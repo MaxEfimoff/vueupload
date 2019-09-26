5dafa888c9129cd06da62513410d375a46d1963e
Max Efimoff <emv3@ya.ru>
2019-09-26 18:30:59 +0400

    image processing added

--- back/index.js
+++ back/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const multer = require('multer');
-
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
+app.use('/static',express.static(path.join(__dirname, 'static')));
 
 const fileFilter = function(req, file, cb) {
   const allowedTypes = ['image/jpeg', 'image/png'];
@@ -16,17 +19,23 @@ const fileFilter = function(req, file, cb) {
   cb(null, true);
 }
 
-const MAX_SIZE = 140000;
+const MAX_SIZE = 1400000;
+
 const upload = multer({
   dest: './uploads',
-  fileFilter,
+  // fileFilter,
   limits: {
     fileSize: MAX_SIZE
   }
 });
-const pureupload = multer({
-  dest: './uploads',
-})
+
+// const pureupload = multer({
+//   dest: './uploads',
+// })
+
+// app.post('/dropzone', pureupload.single('file'), (req, res) => {
+//   res.json({file: req.file});
+// })
 
 // 'file' comes from front/src/SimpleUpload.vue 
 // 33 line formData.append('file', this.file);
@@ -38,9 +47,20 @@ app.post('/multiple', upload.array('files'), (req, res) => {
   res.json({files: req.files});
 })
 
-app.post('/dropzone', pureupload.single('file'), (req, res) => {
-  res.json({file: req.file});
-})
+app.post('/dropzone', upload.single('file'), 
+  async (req, res) => {
+    try {
+      await sharp(req.file.path)
+        .resize(300)
+        .toFile(`./static/${req.file.originalname}`);
+
+      fs.unlink(req.file.path, () => {
+        res.json({file: `/static/${req.file.originalname}`});
+      })
+    } catch(err) {
+      res.status(422).json({err});
+    }
+});
 
 
 app.use(function(err, req, res, next) {
